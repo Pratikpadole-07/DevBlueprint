69f4049371e2e5d99f8ba098260d3c363f0be9c2
Pratikpadole-07 <padolepratik25@gmail.com>
2026-08-12 23:09:03 +0530

    app.js added

---
+++ ECommmerce/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+import authRoutes from "./routes/authRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.use("/api/products", productRoutes);
+
+app.use("/api/cart", cartRoutes);
+
+app.use("/api/orders", orderRoutes);
+
+app.get("/", (req, res) => {
+
+    res.send("E-Commerce Backend Running");
+
+});
+
+export default app;
