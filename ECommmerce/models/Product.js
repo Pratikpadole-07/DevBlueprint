72a29436c52159b93a9a3e126afbcafff95ee2b5
Pratikpadole-07 <padolepratik25@gmail.com>
2026-08-12 23:14:20 +0530

    models added

---
+++ ECommmerce/models/Product.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        description: {
+            type: String,
+            required: true,
+        },
+
+        price: {
+            type: Number,
+            required: true,
+            min: 0,
+        },
+
+        category: {
+            type: String,
+            required: true,
+        },
+
+        stock: {
+            type: Number,
+            required: true,
+            min: 0,
+            default: 0,
+        },
+
+        image: {
+            type: String,
+            default: "",
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export default mongoose.model("Product", productSchema);
